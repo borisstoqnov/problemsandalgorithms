45810944c09d036e5d0c829f62b22bfa0f757f0d
bobi <bobi@malkashtaiga>
2017-06-16 14:08:30 +0300

    first commit

---
+++ lcmandgcd.py
@@ -0,0 +1,22 @@
+# GCD and LCM are not in math module.  They are in gmpy, but these are simple enough:
+
+def gcd(a, b):
+    """Compute the greatest common divisor of a and b"""
+    while b > 0:
+        temp=a
+        a = b
+        b = temp%b
+
+#        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """Compute the lowest common multiple of a and b"""
+    return a * b / gcd(a, b)
+
+listofn = [10,20,30,5]
+result = listofn[0]
+for x in listofn:
+    result = lcm(result,x)
+print(result)

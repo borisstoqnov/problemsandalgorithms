a9764fa125f1543134992f11440ceb3674caa340
borisstoqnov <SPbsroxxbs1>
2017-06-19 14:01:18 +0300

    more

--- isprime.py
+++ isprime.py
@@ -1,4 +1,13 @@
 def is_prime(a):
     return all(a % i for i in range(2, a))
 
-print(is_prime(33)a)
+def is_primsecond(b):
+
+    if b % 2 == 0:
+        return False
+    for i in range(2,b):
+        if b % i == 0:
+            return False
+    else:
+        return True
+print(is_primsecond(2833419889721787128217599))
